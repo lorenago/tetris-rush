33dfbbc92d0bb3f20fd21c20d658af44ecc63760
Lorena González Cordero <lore_383@hotmail.com>
2024-03-15 12:10:47 +0100

    fix(vite-config): Fix vite config to have a bootstrap alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/tetris-rush/"
+    base: "/tetris-rush/",
+    root: path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      }
+    }
 })
