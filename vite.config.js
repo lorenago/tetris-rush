cda7d3e705c9a36de22e69567c64c845724ebe68
Lorena González Cordero <lore_383@hotmail.com>
2024-03-15 12:22:05 +0100

    fix(vite-config): Revert last changes and update main js path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
-const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/tetris-rush/",
-    root: path.resolve(__dirname, 'src'),
-    resolve: {
-      alias: {
-        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      }
-    }
+    base: "/tetris-rush/"
 })
